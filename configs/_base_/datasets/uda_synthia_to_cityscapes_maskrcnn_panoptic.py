5f4567e31cf1ea4efee0254cbcdd1be78ca4c54a
Petros Polydorou <ppolydorou@biwidl202.ee.ethz.ch>
2023-10-25 17:41:21 +0200

    lower resolution

--- configs/_base_/datasets/uda_synthia_to_cityscapes_maskrcnn_panoptic.py
+++ configs/_base_/datasets/uda_synthia_to_cityscapes_maskrcnn_panoptic.py
@@ -6,7 +6,7 @@
 dataset_type = 'CityscapesDataset'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 512)
+crop_size = (256, 256) # (512, 512)
 num_classes = 19
 synthia_train_pipeline = [
                             dict(type='LoadImageFromFile'),
