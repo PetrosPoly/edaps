627eaf54484b7b332aa34ebc4e103d7e9f9667ba
Petros Polydorou <ppolydorou@biwidl212.ee.ethz.ch>
2023-11-29 02:16:23 +0100

    experiments

--- experiments.py
+++ experiments.py
@@ -308,7 +308,7 @@ def generate_experiment_cfgs(id, machine_name):
     # -------------------------------------------------------------------------
     debug = False
     machine = machine_name
-    iters = 50 # 40000
+    iters = 10000 # 40000
     interval = iters
     interval_debug = 3
     uda = 'dacs'
@@ -317,10 +317,10 @@ def generate_experiment_cfgs(id, machine_name):
     # --- Set the debug time configs ---
     # ----------------------------------------
     n_gpus = 1 if debug else 1
-    batch_size = 1 if debug else 1 # 2  # samples_per_gpu
+    batch_size = 1 if debug else 2 # 2  # samples_per_gpu
     workers_per_gpu = 0 if debug else 4 # if 'dacs' in uda else 2
     eval_interval = interval_debug if debug else interval
-    checkpoint_interval = interval_debug if debug else interval
+    checkpoint_interval = interval_debug if debug else 5000 # interval
     ann_dir = 'gtFine_panoptic_debug/cityscapes_panoptic_val' if debug else 'gtFine_panoptic/cityscapes_panoptic_val'
     log_interval = 1 if debug else 50
     debug_img_interval = 1 if debug else 5000
@@ -355,7 +355,7 @@ def generate_experiment_cfgs(id, machine_name):
     num_samples_debug = 12
     gt_dir_instance = 'data/cityscapes/gtFine/val'
     gt_dir_panoptic = 'data/cityscapes/gtFine_panoptic'
-    eval_metric_list = ['mIoU', 'mPQ', 'mAP']
+    eval_metric_list = ['mIoU'] # ['mIoU', 'mPQ', 'mAP']
     mapillary_dataloading_style = 'OURS'
     set_diff_pmult_for_sem_and_inst_heads = False
     semantic_decoder = 'sepaspp'
